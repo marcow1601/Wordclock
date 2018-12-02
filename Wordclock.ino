65428cd1726b2720a73d5413f7bccd3c6c601116
Marco Wagner <marco1601@web.de>
2018-12-02 12:39:02 +0100

    NTP Client Example implemented

--- Wordclock.ino
+++ Wordclock.ino
@@ -1,9 +1,104 @@
+#include <TimeLib.h>
+#include "WifiConfig.h"
+#include <NtpClientLib.h>
+#include <ESP8266WiFi.h>
+
+#ifndef WIFI_CONFIG_H
+#define YOUR_WIFI_SSID "YOUR_WIFI_SSID"
+#define YOUR_WIFI_PASSWD "YOUR_WIFI_PASSWD"
+#endif // !WIFI_CONFIG_H
+
+int8_t timeZone = 1;
+int8_t minutesTimeZone = 0;
+bool wifiFirstConnected = false;
+
+void onSTAConnected (WiFiEventStationModeConnected ipInfo) {
+    Serial.printf ("Connected to %s\r\n", ipInfo.ssid.c_str ());
+}
+
+// Start NTP only after IP network is connected
+void onSTAGotIP (WiFiEventStationModeGotIP ipInfo) {
+    Serial.printf ("Got IP: %s\r\n", ipInfo.ip.toString ().c_str ());
+    Serial.printf ("Connected: %s\r\n", WiFi.status () == WL_CONNECTED ? "yes" : "no");
+    digitalWrite (LED_BUILTIN, LOW); // Turn on LED
+    wifiFirstConnected = true;
+}
+
+void processSyncEvent (NTPSyncEvent_t ntpEvent) {
+    if (ntpEvent) {
+        Serial.print ("Time Sync error: ");
+        if (ntpEvent == noResponse)
+            Serial.println ("NTP server not reachable");
+        else if (ntpEvent == invalidAddress)
+            Serial.println ("Invalid NTP server address");
+    } else {
+        Serial.print ("Got NTP time: ");
+        Serial.println (NTP.getTimeDateString (NTP.getLastNTPSync ()));
+    }
+}
+
+// Manage network disconnection
+void onSTADisconnected (WiFiEventStationModeDisconnected event_info) {
+    Serial.printf ("Disconnected from SSID: %s\n", event_info.ssid.c_str ());
+    Serial.printf ("Reason: %d\n", event_info.reason);
+    digitalWrite (LED_BUILTIN, HIGH); // Turn off LED
+    //NTP.stop(); // NTP sync can be disabled to avoid sync errors
+}
+
+boolean syncEventTriggered = false; // True if a time event has been triggered
+NTPSyncEvent_t ntpEvent; // Last triggered event
+
 void setup() {
-  // put your setup code here, to run once:
+  static WiFiEventHandler e1, e2, e3;
+
+    Serial.begin (115200);
+    Serial.println ();
+    WiFi.mode (WIFI_STA);
+    WiFi.begin (YOUR_WIFI_SSID, YOUR_WIFI_PASSWD);
+
+    pinMode (LED_BUILTIN, OUTPUT); // Onboard LED
+    digitalWrite (LED_BUILTIN, HIGH); // Switch off LED
+
+    NTP.onNTPSyncEvent ([](NTPSyncEvent_t event) {
+        ntpEvent = event;
+        syncEventTriggered = true;
+    });
+
+    e1 = WiFi.onStationModeGotIP (onSTAGotIP);// As soon WiFi is connected, start NTP Client
+    e2 = WiFi.onStationModeDisconnected (onSTADisconnected);
+    e3 = WiFi.onStationModeConnected (onSTAConnected);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  static int i = 0;
+    static int last = 0;
+
+    if (wifiFirstConnected) {
+        wifiFirstConnected = false;
+        NTP.begin ("pool.ntp.org", timeZone, true, minutesTimeZone);
+        NTP.setInterval (63);
+    }
+
+    if (syncEventTriggered) {
+        processSyncEvent (ntpEvent);
+        syncEventTriggered = false;
+    }
+
+    if ((millis () - last) > 5100) {
+        //Serial.println(millis() - last);
+        last = millis ();
+        Serial.print (i); Serial.print (" ");
+        Serial.print (NTP.getTimeDateString ()); Serial.print (" ");
+        Serial.print (NTP.isSummerTime () ? "Summer Time. " : "Winter Time. ");
+        Serial.print ("WiFi is ");
+        Serial.print (WiFi.isConnected () ? "connected" : "not connected"); Serial.print (". ");
+        Serial.print ("Uptime: ");
+        Serial.print (NTP.getUptimeString ()); Serial.print (" since ");
+        Serial.println (NTP.getTimeDateString (NTP.getFirstSync ()).c_str ());
+
+        i++;
+    }
+    delay (0);
 
 }
